65bcc8c2f85117a49a708f829428a24568c3a404
U-KC\wuhongsong <wuhongsong@IT01010010356.kc.net>
2014-07-24 14:41:48 +0800

    step_2

--- js/calendar.js
+++ js/calendar.js
@@ -60,15 +60,22 @@ Calendar.prototype = {
 		
 		this.$calendar = $('<div class="calendar"></div>');
 		var oDate = new Date();
+		var year = oDate.getFullYear();
+		var month = oDate.getMonth()+1;
+		var date = oDate.getDate()
 		
 		var monthDays = new Date(oDate.getFullYear(), (oDate.getMonth()+1), 0).getDate();
+		    oDate.setDate(1);
 		
-		var html = '<div class="calendar_title"><div class="prev_year">&lt;&lt;</div><div class="prev_month">&lt;</div><div class="data_now">'+oDate.getFullYear()+'-'+toDouble(oDate.getMonth()+1)+'-'+toDouble(oDate.getDate())+'</div><div class="next_month">&gt;</div><div class="next_year">&gt;&gt;</div></div><div class="calendar_box"><div class="calendar_table"><table cellpadding="0" cellspacing="0"><thead><td>日</td><td>一</td><td>二</td><td>三</td><td>四</td><td>五</td><td>六</td></thead><tbody>'
+		var day = oDate.getDay();
+		console.log(day)
 		
-		for(var i=1;i<=monthDays;){
+		var html = '<div class="calendar_title"><div class="prev_year">&lt;&lt;</div><div class="prev_month">&lt;</div><div class="data_now">'+year+'-'+toDouble(month)+'-'+toDouble(date)+'</div><div class="next_month">&gt;</div><div class="next_year">&gt;&gt;</div></div><div class="calendar_box"><div class="calendar_table"><table cellpadding="0" cellspacing="0"><thead><td>日</td><td>一</td><td>二</td><td>三</td><td>四</td><td>五</td><td>六</td></thead><tbody>'
+		
+		for(var i=1;i<=6;i++){
 			html +='<tr>'
 			for(var j=0;j<7;j++){
-				html += '<td>'+ (i++)+'</td>'
+				html += '<td></td>'
 			}
 			html+='</tr>';
 		}
@@ -100,4 +107,44 @@ function toDouble(num){
 		return num;
 	}
 			
+}
+
+function datePos(d){
+	switch(d){
+		case 0:
+			for(var i=0;i<dayNum;i++){
+				aTd[i+6].innerHTML = i+1;
+			}
+		break;
+		case 1:
+			for(var i=0;i<dayNum;i++){
+				aTd[i].innerHTML = i+1;
+			}
+		break;
+		case 2:
+			for(var i=0;i<dayNum;i++){
+				aTd[i+1].innerHTML = i+1;
+			}
+		break;
+		case 3:
+			for(var i=0;i<dayNum;i++){
+				aTd[i+2].innerHTML = i+1;
+			}
+		break;
+		case 4:
+			for(var i=0;i<dayNum;i++){
+				aTd[i+3].innerHTML = i+1;
+			}
+		break;
+		case 5:
+			for(var i=0;i<dayNum;i++){
+				aTd[i+4].innerHTML = i+1;
+			}
+		break;
+		case 6:
+			for(var i=0;i<dayNum;i++){
+				aTd[i+5].innerHTML = i+1;
+			}
+		break;
+	}
 }
